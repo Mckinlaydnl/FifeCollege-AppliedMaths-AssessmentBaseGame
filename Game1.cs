d8df56ad745bf2b09352e4189edea69346bda834
Mckinlaydnl <43002081+Mckinlaydnl@users.noreply.github.com>
2019-06-18 11:10:22 +0100

    Updated task 6 for remediation

--- Game1.cs
+++ Game1.cs
@@ -31,6 +31,19 @@ namespace Assessment
         int doorSequenceTimer;
         int doorSequenceFinalTime = 2500;
 
+        // Task 2 variables
+        Vector3 acceleration_old = Vector3.Zero;
+        Vector3 velocity_old = Vector3.Zero;
+        Vector3 position_older = Vector3.Zero;
+        Vector3 position_old = Vector3.Zero;
+
+        
+
+
+       
+
+        // Add a variable to keep track of when rock started falling
+        float rockFallStartTime = 0f;
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -69,7 +82,7 @@ namespace Assessment
             BoundingRenderer.InitializeGraphics(graphics.GraphicsDevice);
             spriteBatch = new SpriteBatch(GraphicsDevice);
             player.LoadModel(Content, "Ship");
-            player.rotation = new Vector3(1.5f, 0f, 0f);
+            player.rotation = new Vector3(0f, 0f, 0f);
             player.position.X = 0;
             player.position.Y = 0;
             player.position.Z = 0;
@@ -115,7 +128,7 @@ namespace Assessment
             {
                 case IntegrationMethod.ForwardEuler:
                     //// This method is deprecated due to stability issues.
-                    player.position += player.velocity * dt;
+                    player.position -= player.velocity * dt;
                     player.velocity += acceleration * dt;
 
                     break;
@@ -126,10 +139,37 @@ namespace Assessment
                 //
                 ///////////////////////////////////////////////////////////////////
                 case IntegrationMethod.LeapFrog:
+                    // Calculate the velocity half way through the frame using the acceleration from the last frame
+                    Vector3 velocity_half = velocity_old + (acceleration_old * dt) * 0.5f;
+                    // Calculate new positions using this halfway velocity
+                    player.position = position_old + velocity_half * dt;
+                    // Calculate a new velocity for this frames accelaration and half a time step
+                    player.velocity = velocity_half + (acceleration * dt) * 0.5f;
+
+                    // Apply drag to the movement AKA Friction
+                    player.velocity *= 0.99f;
                     break;
+
                 case IntegrationMethod.Verlet:
+                    // Update the acceleration
+                    if (position_older != position_old)
+                    {
+                        Vector3 drag = position_older - position_old;
+                        drag.Normalize();
+                        drag *= 0.0001f;
+                        acceleration += drag;
+
+                    }
+
+                    player.position = 2 * position_old - position_older + acceleration * dt * dt;
+
                     break;
             }
+            // Move current values to old values
+            acceleration_old = acceleration;
+            velocity_old = player.velocity;
+            position_older = position_old;
+            position_old = player.position;
         }
 
 
@@ -182,16 +222,40 @@ namespace Assessment
             if (player.hitBox.Intersects(TriggerBoxRockFall) && !rockFalling)
             {
                 rockFalling = true;
-                rock.velocity = new Vector3(0, 0.2f, 0);
+
+                    // Assign rock fall start time 
+                    rockFallStartTime = (float)gameTime.TotalGameTime.TotalSeconds;
+                
             }
             if (rockFalling)
             {
-                Vector3 gravity = new Vector3(0, -0.01f, 0);
+                Vector3 gravity = new Vector3(0, -10f, 0);
                 ///////////////////////////////////////////////////////////////////
                 //
                 // CODE FOR TASK 4 SHOULD BE ENTERED HERE
                 //
                 ///////////////////////////////////////////////////////////////////
+
+                // Calculate time since rocks started falling
+                // Calculate rocks new y position using the derived equation
+                // Stop when you reach the ground (0)
+
+                
+                    float timeSinceRockFall = (float)gameTime.TotalGameTime.TotalSeconds - rockFallStartTime;
+                    Vector3 rockStartPosition = new Vector3(25, 60, -50);
+
+                    rock.position.Y = gravity.Y * timeSinceRockFall * timeSinceRockFall / 2f + rock.velocity.Y * timeSinceRockFall + rockStartPosition.Y;
+                    if (rock.position.Y < 0f)
+                    {
+                        rock.position.Y = 0;
+                        rockFallStartTime = 0f;
+                        
+                    }
+                
+
+
+
+
             }
             if (player.hitBox.Intersects(TriggerBoxDoorOpen))
             {
@@ -207,6 +271,18 @@ namespace Assessment
                 // CODE FOR TASK 5 SHOULD BE ENTERED HERE
                 //
                 ///////////////////////////////////////////////////////////////////
+                ///
+
+                // Easing
+                // Get game time
+                doorSequenceTimer += gameTime.ElapsedGameTime.Milliseconds;
+                if (doorSequenceTimer >= doorSequenceFinalTime)
+                {
+                    // reset timer
+                    doorSequenceTimer = doorSequenceFinalTime;
+                }
+                newPos = CubicInterpolation(doorStartPoint, doorEndPoint, (float)doorSequenceTimer, (float)doorSequenceFinalTime);
+                door.SetUpVertices(newPos);
             }
 
 
@@ -215,13 +291,60 @@ namespace Assessment
 
         private void ElasticCollision(basicCuboid w)
         {
-            player.velocity *= -1;
-            player.position = player.storedPos;
+            //player.velocity *= -1;
+            //player.position = player.storedPos;
             ///////////////////////////////////////////////////////////////////
             //
             // CODE FOR TASK 7 SHOULD BE ENTERED HERE
             //
             ///////////////////////////////////////////////////////////////////
+            ///
+            // Need the perpindicular vector the the face of the box we hit
+            // To do this, we need TWO vectors ON the face of the box we hit
+            Vector3 faceVector1;
+            Vector3 faceVector2;
+
+            // Get the corners of the box we hit so we can calculate the face vectors
+            Vector3[] corners = w.collisionbox.GetCorners();
+            // Corners of the box faces that are perpendicular to the z axis (facing along the z axis)
+            // 0-3 is the near face, 4-7 is the far face
+            // Start upper left, then upper right, then lower right, then lower left (clockwise)
+
+            // Move back our player to their previous position (so they aren't inside the box)
+            player.position = player.storedPos;
+
+            // Is the player's new position overlapping in the X direction
+            if ((player.hitBox.Min.X - player.velocity.X) > w.collisionbox.Max.X 
+                || (player.hitBox.Max.X - player.velocity.X) < w.collisionbox.Min.X)
+            {
+                // Overlapping from right or left!
+
+                // Line from front top left going to the top right
+                faceVector1 = corners[1] - corners[6];
+                // Line from front top left going to the bottom right
+                faceVector2 = corners[2] - corners[6];
+
+            }
+            // If we are NOT overlapping right or left, we are overlapping front/back (z axis)
+            else
+            {
+                // Overlapping front or back!
+                // Line from front top left going to the top right
+                faceVector1 = corners[1] - corners[0];
+                // Line from front top left going to the bottom right
+                faceVector2 = corners[2] - corners[0];
+
+            }
+            // (we ignore the possibility of a y direction)
+
+            // Get a cross product between these two vectors to define a normal perpindicular to the plane (face)
+            Vector3 normal = Vector3.Cross(faceVector1, faceVector2);
+            // Make it a unit vector (length 1)
+            normal.Normalize();
+
+            // Use this normal vector to reflect the player's velocity
+            // (this uses a dot product equation internally)
+            player.velocity = Vector3.Reflect(player.velocity, normal);
         }
         ///////////////////////////////////////////////////////////////////
         //
@@ -229,9 +352,30 @@ namespace Assessment
         //
         ///////////////////////////////////////////////////////////////////
         public Vector3 CubicInterpolation(Vector3 initialPos, Vector3 endPos, float
-        time)
+        time, float duration)
         {
-            return new Vector3(0, 0, 0);
+            // Calculate an independent variable time as a proportion
+            // Between (0 and 1)
+
+            float t = time / duration;
+
+            // Calculate p aka distance traveled from start
+            // Using a derived quadratic function
+            // Produces a fraction of the complete distance 
+            // This is our scaling factor
+            float p = -2 * t * t * t + 3 * t * t;
+
+            // Determine the total distance to be traveled
+            Vector3 totalDistance = endPos - initialPos;
+
+            //Determine the distance traveled
+            // By scaling the total distance by our scaling factor
+            Vector3 distanceTravelled = totalDistance * p;
+
+            // Determine the new position by adding the distance traveled to the start point
+            Vector3 newPosition = initialPos + distanceTravelled;
+
+            return newPosition;
         }
 
         /// <summary>
